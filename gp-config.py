746b53625539a4e7fe075901ca948d7bbf369751
newsyd04 <newsyd04@gmail.com>
2025-11-18 15:08:13 +0000

    inital config

--- gp-config.py
+++ gp-config.py
@@ -7,7 +7,6 @@ import functools
 import numpy as np
 
 # ---- FEATURE DEFINITIONS ----
-# These are the columns your dataset must contain.
 FEATURES = [
     "delta_net_rating_3yr",
     "delta_TS",
